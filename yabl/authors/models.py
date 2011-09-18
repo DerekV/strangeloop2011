040515d59c76488ef706149b5f2442dc8f9bec2c
Derek VerLee <dverlee@riis.com>
2011-09-18 17:22:06 -0400

    create authors app

---
+++ yabl/authors/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    bio = models.TextField(blank=True)
--- yabl/settings.py
+++ yabl/settings.py
@@ -12,8 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/derekverlee
-/tmp/yabl.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/derekverlee/tmp/yabl.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yabl.authors'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
